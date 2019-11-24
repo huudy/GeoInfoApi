12810bad047822593f1c8176fa3acca1131c5025
Home <Home@DESKTOP-JBEJJKS>
2019-11-24 22:14:12 +0100

    azure deployment ready + starting with tests

---
+++ GeoIpApi.Test/TestGeoInfo.cs
@@ -0,0 +1,40 @@
+﻿using GeoIpApi.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoIpApi.Test
+{
+    [TestClass]
+    class TestGeoInfo
+    {
+        [TestMethod]
+        public void GetAllGeoInfo()
+        {
+
+        }
+
+        [TestMethod]
+        public async Task GetGeoInfoByIdAsyc()
+        {
+            var controller = new GeoInfosController();
+            controller.ge
+        }
+
+        [TestMethod]
+        public void CreateGeoInfo()
+        {
+
+        }
+
+        [TestMethod]
+        public void DeleteGeoInfoById()
+        {
+
+        }
+
+    }
+}
--- GeoIpApi/App_Start/WebApiConfig.cs
+++ GeoIpApi/App_Start/WebApiConfig.cs
@@ -9,18 +9,9 @@ namespace GeoIpApi
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
-
-            // Web API routes
+        {           
             config.MapHttpAttributeRoutes();
-
-            //config.Routes.MapHttpRoute(
-            //    name: "DefaultApi",
-            //    routeTemplate: "api/{controller}/{id}",
-            //    defaults: new { id = RouteParameter.Optional }
-            //);
-
+            
             var jsonFormatter = config.Formatters.JsonFormatter;
             jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             config.Formatters.Remove(config.Formatters.XmlFormatter);
